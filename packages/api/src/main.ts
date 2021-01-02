b8de4eda2f98599d8dfa44de3d2a35fd6f3fc124
auctumnus <auctumnus@users.noreply.github.com>
2021-01-01 23:13:08 -0600

    ✨ add GNU Terry Pratchett header http://www.gnuterrypratchett.com/index.php

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -3,11 +3,13 @@ import { AppModule } from './app.module'
 import { ValidationPipe } from './validationPipe'
 import { TrimPipe } from './trimPipe'
 import { classTransformerInterceptor } from './classTransformerInterceptor'
+import { ClacksMiddleware } from './clacks.middleware'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(new TrimPipe(), ValidationPipe)
   app.useGlobalInterceptors(new classTransformerInterceptor())
+  app.use(ClacksMiddleware)
   await app.listen(3000)
 }
 bootstrap()
