1a2e543dc2497a07fba526a656b327ae10ff33e1
auctumnus <auctumnus@users.noreply.github.com>
2021-06-04 22:01:20 -0500

    🚑️ fix frontend build by removing vite-ssg

--- packages/web/src/main.ts
+++ packages/web/src/main.ts
@@ -1,12 +1,18 @@
 import './style.css'
 import 'reset.css'
 import 'virtual:windi.css'
-import { ViteSSG } from 'vite-ssg'
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 // @ts-ignore
 import routes from 'virtual:generated-pages'
 import App from './App.vue'
 import { key, store } from './store'
 
-export const createApp = ViteSSG(App, { routes }, ({ app }) => {
-  app.use(store, key)
-})
+const history = createWebHistory()
+
+const router = createRouter({ routes, history })
+
+const app = createApp(App)
+
+app.use(store, key)
+app.use(router)
