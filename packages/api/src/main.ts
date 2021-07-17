b74df9d696b9a34c9e4ab35b1d21ff031ef8a67b
auctumnus <auctumnus@users.noreply.github.com>
2021-07-17 11:51:47 -0500

    update the rest of nest

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core'
+import { INestApplication } from '@nestjs/common'
 import { AppModule } from './app.module'
 import { ValidationPipe } from './validationPipe'
 import { TrimPipe } from './trimPipe'
@@ -9,8 +10,9 @@ import { setupBuckets } from './s3'
 import { SwaggerModule } from '@nestjs/swagger'
 import { getDocument } from './swagger'
 
-export async function bootstrap(port: number, logger: boolean) {
+export async function bootstrap(port: number, shouldLog: boolean): Promise<INestApplication> {
   await setupBuckets()
+  const logger = shouldLog ? console : false
   const app = await NestFactory.create(AppModule, { logger })
   app.useGlobalPipes(new TrimPipe(), ValidationPipe)
   app.useGlobalInterceptors(new classTransformerInterceptor())
