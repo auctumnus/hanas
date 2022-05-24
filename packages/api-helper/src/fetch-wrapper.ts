b3a51c6eff55ccdc0d68097507ca8435ce4d2524
auctumnus <auctumnus@users.noreply.github.com>
2022-05-23 20:50:21 -0500

    i promise this is the last one-off weird commit

--- packages/api-helper/src/fetch-wrapper.ts
+++ packages/api-helper/src/fetch-wrapper.ts
@@ -13,18 +13,17 @@ export type WrapperBody = FetchBody & {
   params?: Record<string, string>
 }
 
-export interface IHanasError<Status, Message> {
-  error: true
-  data: {
-    status: Status
-    message: Message
-    issues: Status extends 400 ? { message: string }[] : never
-  }
-}
-
 export type HanasError<Status = number, Message = string> = Status extends never
   ? never
-  : IHanasError<Status, Message>
+  : {
+      error: true
+      data: {
+        status: Status
+        message: Message
+        issues: Status extends 400 ? { message: string }[] : never
+      }
+    }
+
 /**
  * Simple fetch wrapper.
  * @typeparam ResponseType The type to cast the response to. If not given, defaults to any.
@@ -57,6 +56,7 @@ export const w = async <
     url + '?' + new URLSearchParams(opts.params) + '',
     options
   )
+
   if (!res.ok) {
     try {
       const error = await res.json()
@@ -69,177 +69,33 @@ export const w = async <
   return res.json() as Promise<ResponseType>
 }
 
-const _awf =
-  (client: HanasClient) =>
-  <ResponseType = any, ErrorStatus = number, ErrorMessage = string>(
+/**
+ * Creates a fetch wrapper that can use auth information.
+ * @param client The client to use auth/URL info from.
+ * @returns
+ */
+export const makeAuthedWrapper = (client: HanasClient) => {
+  /**
+   * See the included wrapper, except the path is a string and
+   * an endpoint on
+   * @see {@link w}
+   */
+  const wrapped = <
+    ResponseType = any,
+    ErrorStatus = number,
+    ErrorMessage = string
+  >(
     path: string,
     opts: WrapperBody = {}
-  ) =>
-  (otherOpts: WrapperBody = {}) =>
-    w<ResponseType, ErrorStatus, ErrorMessage>(
-      new URL(path, client.options.hanasURL),
-      {
-        ...opts,
-        ...otherOpts,
-        ...(client.options.token
-          ? {
-              headers: {
-                'X-Session-Token': client.options.token,
-                ...opts.headers,
-                ...otherOpts.headers,
-              },
-            }
-          : {}),
-      }
-    )
-
-/**
- * Creates a fetch wrapper that uses URL and auth information from the client.
- * @param client The client to use.
- * @returns A fetch wrapper.
- * @see {@link w}
- */
-export const authWrapperFactory =
-  (client: HanasClient) =>
-  <ResponseType = any, ErrorStatus = number, ErrorMessage = string>(
-    path: string,
-    opts: WrapperBody = {}
-  ) =>
-    _awf(client)<ResponseType, ErrorStatus, ErrorMessage>(path, opts)()
-
-/**
- * Contains information for a paginated response.
- * @typeparam ResponseType The type of the response data.
- */
-export class PagedResponse<
-  ResponseType = any,
-  ErrorStatus = number,
-  ErrorMessage = string
-> {
-  constructor(
-    private readonly _data: ResponseType[],
-    private readonly nextCursor: string | null,
-    private readonly prevCursor: string | null,
-    private readonly client: HanasClient,
-    private readonly path: string,
-    private readonly opts: WrapperBody
-  ) {}
-
-  public readonly error = false
-
-  /**
-   * Returns the next page for this paginated endpoint.
-   * @param take The number of results to take.
-   * @returns Either another paginated response, null (for no next page), or an error.
-   */
-  async next(
-    take = 10
-  ): Promise<
-    | PagedResponse<ResponseType, ErrorStatus, ErrorMessage>
-    | HanasError<ErrorStatus, ErrorMessage>
-    | null
-  > {
-    if (this.nextCursor === null) {
-      return null
+  ) => {
+    if (!opts.headers) {
+      opts.headers = {}
     }
-
-    if (!this.opts.params) {
-      this.opts.params = {}
+    if (client.options.token) {
+      ;(opts.headers as Record<string, string>)['X-Session-Token'] =
+        client.options.token
     }
-
-    this.opts.params.cursor = this.nextCursor
-    this.opts.params.take = take + ''
-
-    return paginated(this.client)<ResponseType, ErrorStatus, ErrorMessage>(
-      this.path,
-      this.opts
-    )
+    return w(new URL(path, client.options.hanasURL), opts)
   }
-
-  /**
-   * Returns the previous page for this paginated endpoint.
-   * @param take The number of results to take.
-   * @returns Either another paginated response, null (for no previous page), or an error.
-   */
-  previous(
-    take = 10
-  ): Promise<
-    | PagedResponse<ResponseType, ErrorStatus, ErrorMessage>
-    | HanasError<ErrorStatus, ErrorMessage>
-    | null
-  > {
-    if (this.prevCursor === null) {
-      return null
-    }
-
-    if (!this.opts.params) {
-      this.opts.params = {}
-    }
-
-    this.opts.params.cursor = this.prevCursor
-    this.opts.params.take = take + ''
-
-    return paginated(this.client)<ResponseType, ErrorStatus, ErrorMessage>(
-      this.path,
-      this.opts
-    )
-  }
-
-  /**
-   * @returns The data from this response.
-   */
-  getData() {
-    return this._data
-  }
+  return wrapped
 }
-
-export interface Paginated<T> {
-  data: T[]
-  cursor: {
-    next: string | null
-    previous: string | null
-  }
-}
-
-/**
- * Creates a fetch wrapper with a paginated response.
- * @param client The client to use for the fetch wrapper.
- * @returns The fetch wrapper.
- * @see {@link w}
- * @see {@link authWrapperFactory}
- */
-export const paginated =
-  (client: HanasClient) =>
-  async <ResponseType = any, ErrorStatus = number, ErrorMessage = string>(
-    path: string,
-    opts: WrapperBody = {}
-  ): Promise<
-    | PagedResponse<ResponseType, ErrorStatus, ErrorMessage>
-    | HanasError<ErrorStatus, ErrorMessage>
-    | null
-  > => {
-    const request = _awf(client)<
-      Paginated<ResponseType>,
-      ErrorStatus,
-      ErrorMessage
-    >(path, opts)
-    const initialResponse = await request()
-
-    if ('error' in initialResponse && initialResponse.error) {
-      return initialResponse
-    }
-
-    const {
-      data,
-      cursor: { next, previous },
-    } = initialResponse as Paginated<ResponseType>
-
-    return new PagedResponse<ResponseType, ErrorStatus, ErrorMessage>(
-      data,
-      next,
-      previous,
-      client,
-      path,
-      opts
-    )
-  }
